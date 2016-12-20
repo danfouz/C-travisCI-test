e604565805f8aa481ac0be0ad5d2e9bd813e8369
Dan <danfouzi@gmail.com>
2016-12-20 17:43:17 +0100

    Test Eliminados del codigo

--- src/fib_complexN.c
+++ src/fib_complexN.c
@@ -21,45 +21,6 @@ int fib2(int n){
   return j;
 }
 
-void test2(){
-int n,j,umbral=500,repeticiones=1000;char flag;
-double ajustada,sobrestimada,subestimada,t1,t2,ta,tb,tu,t;
-  printf("\nTEST 2");
-  printf("\n%10s%15s%15s%13s%14s","n","t(n)","Subestimada","Ajustada","Sobrestimada");
-  printf("\n%40s%13s%15s\n","t(n)/n^0.8","t(n)/n","t(n)/n*log(n)");
-  for(j=0;j<70;j++){
-    printf("-");
-  }
-  printf("\n");
-
-  for (n=1000;n<=10000000;n*=10){
-    t1=microsegundos();
-    flag=' '; fib2(n);
-    t2=microsegundos();
-    t=t2-t1;
-    if (t < umbral) {
-      flag='*';
-      int k;
-      ta=microsegundos();
-      for (k=0;k<repeticiones;k++){
-        fib2(n);
-      }
-      tb=microsegundos();
-      tu=tb-ta;
-      ta=microsegundos();
-      for (k=0;k<repeticiones;k++){
-      }
-      tb=microsegundos();
-      t=tb-ta;
-      t=(tu-t)/repeticiones; 
-    }
-    ajustada=t/n;
-    sobrestimada=t/(n*(log(n)));
-    subestimada=t/pow(n,0.8);
-    printf("%10d%14.4f%3c%13.6f%13.6f%13.6f\n",n,t,flag,subestimada,ajustada,sobrestimada );
-  }
-}
-
 
 
 
--- src/fib_complexlogN.c
+++ src/fib_complexlogN.c
@@ -29,43 +29,6 @@ int i,j,k,h,t;
   return j;
 }
 
-void test3(){
-int n,j,umbral=500,repeticiones=1000;char flag;
-double ajustada,sobrestimada,subestimada,t1,t2,ta,tb,tu,t;
-  printf("\nTEST 3");
-  printf("\n%10s%15s%15s%13s%14s","n","t(n)","Subestimada","Ajustada","Sobrestimada");
-  printf("\n%42s%12s%13s\n","t(n)/sqrt(log(n))","t(n)/log n","t(n)/n^0.5");
-  for(j=0;j<70;j++){
-    printf("-");
-  }
-  printf("\n");
 
-  for (n=1000;n<=10000000;n*=10){
-    t1=microsegundos();
-    flag=' '; fib3(n);
-    t2=microsegundos();
-    t=t2-t1;
-    if (t < umbral) {
-      flag='*'; 
-      int k;
-      ta=microsegundos();
-      for (k=0;k<repeticiones;k++){
-        fib3(n);
-      }
-      tb=microsegundos();
-      tu=tb-ta;
-      ta=microsegundos();
-      for (k=0;k<repeticiones;k++){
-      }
-      tb=microsegundos();
-      t=tb-ta;
-      t=(tu-t)/repeticiones;
-    }
-    ajustada=t/log(n); 
-    sobrestimada=t/pow(n,0.5); 
-    subestimada=t/sqrt(log(n));
-    printf("%10d%14.4f%3c%13.6f%13.6f%13.6f\n",n,t,flag,subestimada,ajustada,sobrestimada );
-	} 
-}
 
 
